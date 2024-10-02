8a44434957e83403f994ef63e70d45a9ff437812
Ayesha0900 <ayesha.a.ghaffar07@gmail.com>
2024-10-02 23:44:50 +0500

    Add files via upload
    Palendrome Words and Random Password generator

---
+++ palindrome word & random password/script.js
@@ -0,0 +1,32 @@
+// Palendrome word
+
+
+
+// var word = prompt("Enter your word");
+// var check = "";
+
+// for ( var i = word.length -1 ; i>=0; i--){
+//     check += word[i]
+// }
+
+// if( word === check){
+//     console.log(`${word} is palindrome word`);
+// }
+// else{
+//     console.log(`${word} is not palendrome word`);  
+// };
+
+
+
+// Random password generator
+
+var random = Math.random ();
+var updateRandom = random * 23478;
+var round = Math.floor(updateRandom);
+console.log(round);
+
+
+
+
+
+
